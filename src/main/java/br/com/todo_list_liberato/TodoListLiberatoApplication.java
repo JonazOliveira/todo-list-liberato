49a03a601dd3c2e64e7546894920ae14d2714367
JonazOliveira <jonasdeoliveira91@gmail.com>
2025-08-04 20:53:36 -0300

    Base

---
+++ src/main/java/br/com/todo_list_liberato/TodoListLiberatoApplication.java
@@ -0,0 +1,13 @@
+package br.com.todo_list_liberato;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoListLiberatoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoListLiberatoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/todo_list_liberato/TodoListLiberatoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.todo_list_liberato;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoListLiberatoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
